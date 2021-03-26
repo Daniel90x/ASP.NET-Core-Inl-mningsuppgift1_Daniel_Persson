f4047c37926cedb6f4a6062fc919c613bd4169d0
Daniel Persson <daniel.persson18@learnet.se>
2021-03-26 17:02:18 +0100

    MyEvents Addad och WiP ändrat länken i _Layout så den länkar MyEvents till MyEvents

---
+++ ASP.NET Core Inlämningsuppgift1_Daniel_Persson/Pages/MyEvents.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ASP.NET_Core_Inlämningsuppgift1_Daniel_Persson.Data;
+using ASP.NET_Core_Inlämningsuppgift1_Daniel_Persson.Models;
+
+namespace ASP.NET_Core_Inlämningsuppgift1_Daniel_Persson.Pages
+{
+
+    public class MyEventsModel : PageModel
+    {
+
+
+
+        private readonly ASP.NET_Core_Inlämningsuppgift1_Daniel_Persson.Data.EventContext _context;
+
+
+        public MyEventsModel(ASP.NET_Core_Inlämningsuppgift1_Daniel_Persson.Data.EventContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public List<Event> Event { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Event = await _context.Event.ToListAsync();
+
+            return Page();
+        }
+    }
+    
+}
+
